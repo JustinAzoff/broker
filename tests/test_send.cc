45a866d6b6f2bfb4ba369d4ad65fc8cbed9997ad
Justin Azoff <justin.azoff@gmail.com>
2016-02-05 15:11:33 -0500

    simplify sender. send 1k of data per msg.

--- tests/test_send.cc
+++ tests/test_send.cc
@@ -12,6 +12,8 @@
 
 using namespace std;
 
+const auto padding = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl";
+
 int main(int argc, char** argv)
 {
     broker::init();
@@ -20,31 +22,18 @@ int main(int argc, char** argv)
     sender.peer("127.0.0.1", 9999);
 
     bool ok = false;
+    while(!ok) {
+        std::cout << "waiting for connection..." << endl;
+        auto conn_status = sender.outgoing_connection_status().need_pop().front();
+        ok = conn_status.status == broker::outgoing_connection_status::tag::established;
+    }
+    std::cout << "Connected!" << endl;
 
     for(int i = 0; i < 1000000; i++) {
-        if(ok) {
-            sender.send("topic", broker::message{i});
-        }
-
-        if(!ok) {
-            auto conn_status = sender.outgoing_connection_status().need_pop().front();
-            ok = conn_status.status == broker::outgoing_connection_status::tag::established;
-            if(ok) {
-                std::cout << "established connection to: " << conn_status.peer_name << std::endl;
-            } else {
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            }
-        } else if(ok && i % 6000 == 0) {
+        sender.send("topic", broker::message{padding, i});
+        if (i % 6000 == 0) {
             std::cout << i << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            auto conn_status = sender.outgoing_connection_status().want_pop();
-            for ( auto cs : conn_status ) {
-                if ( cs.status != broker::outgoing_connection_status::tag::established ) {
-                    std::cout << "connection error" << std::endl;
-                    ok = false;
-                }
-            }
+            //std::this_thread::sleep_for(std::chrono::milliseconds(500));
         }
-
     }
 }
